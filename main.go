53f76e61f4533a8c3c45c9feb858ade32d5d5b3a
Your Name <you@example.com>
2018-04-12 13:46:17 -0600

    change connection to TCP and quit HTTP connection

--- main.go
+++ main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
 	"io"
 	"log"
-	"net/http"
+	"net"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
-	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
+	"github.com/gorilla/mux"
 )
 
 type Block struct {
@@ -72,90 +74,83 @@ func replaceChain(newBlocks []Block) {
 	}
 }
 
-func  run() error {
-	mux := makeMuxRouter()
-	httpAddr := os.Getenv("ADDR")
-	log.Println("Listening on ",os.Getenv("ADDR"))
-	s := &http.Server{
-		Addr: ":"+httpAddr,
-		Handler: mux,
-		ReadTimeout: 10*time.Second,
-		WriteTimeout: 10*time.Second,
-		MaxHeaderBytes: 1<<20,
-	}
+var bcServer chan []Block
 
-	if err := s.ListenAndServe(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func makeMuxRouter() http.Handler  {
-	muxRouter := mux.NewRouter()
-	muxRouter.HandleFunc("/",handleGetBlockchain).Methods("GET")
-	muxRouter.HandleFunc("/",handleWriteBlock).Methods("POST")
-	return muxRouter
-}
-
-func handleGetBlockchain(w http.ResponseWriter, r *http.Request){
-	bytes, err := json.MarshalIndent(Blockchain,""," ")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	io.WriteString(w,string(bytes))
-}
-
-func handleWriteBlock(w http.ResponseWriter, r *http.Request){
-	var m Message
-
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&m); err != nil {
-		respondWhitJSON(w,r,http.StatusBadRequest, r.Body)
-		return
-	}
-	defer r.Body.Close()
-
-	newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], m.BPM)
-	if err != nil {
-		respondWhitJSON(w,r, http.StatusInternalServerError,m)
-		return
-	}
-
-	if isBlockValid(newBlock,Blockchain[len(Blockchain)-1]){
-		newBlockchain := append(Blockchain,newBlock)
-		replaceChain(newBlockchain)
-		spew.Dump(Blockchain)
-	}
-
-	respondWhitJSON(w,r, http.StatusCreated,newBlock)
-
-}
-
-
-func respondWhitJSON(w http.ResponseWriter, r *http.Request, code int , payload interface{}) {
-	response, err := json.MarshalIndent(payload, "", " ")
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("HTTP 500: Internal Server Error"))
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(response)
-}
-
-func main(){
+func main()  {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	bcServer = make(chan []Block)
+
+	//create genesis block
+	t := time.Now()
+	genesisBlock := Block{0,t.String(),0,"",""}
+	spew.Dump(genesisBlock)
+	Blockchain = append(Blockchain, genesisBlock)
+
+	// start TCP and serve TCP server
+	server, err := net.Listen("tcp",":"+os.Getenv("ADDR"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer server.Close()
+
+	for {
+		conn,err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn){
+	defer conn.Close()
+
+	io.WriteString(conn, "Enter a new BPM:")
+
+	scanner := bufio.NewScanner(conn)
+
+	// take in BPM from stdin and add it to blockchain after conducting necessary validation
 	go func() {
-		t := time.Now()
-		genesisBlock := Block{0, t.String(),0,"",""}
-		spew.Dump(genesisBlock)
-		Blockchain = append(Blockchain, genesisBlock)
+		for scanner.Scan(){
+			bpm, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Printf("%v not a number: %v",scanner.Text(),err)
+				continue
+			}
+			newBlock, err := generateBlock(Blockchain[len(Blockchain)-1],bpm)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+				newBlockchain := append(Blockchain,newBlock)
+				replaceChain(newBlockchain)
+			}
+
+			bcServer <- Blockchain
+			io.WriteString(conn, "\nEnter a new BPM")
+		}
 	}()
-	log.Fatal(run())
+
+
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			output, err := json.Marshal(Blockchain)
+			if err != nil {
+				log.Fatal(err)
+			}
+			io.WriteString(conn,string(output))
+		}
+	}()
+
+	for _ = range bcServer{
+		spew.Dump(Blockchain)
+	}
+
 }
